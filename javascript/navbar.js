569456e5cdd9bef30757d3e1b2387da9b15a33b2
georgiazrk <102087136+georgiazrk@users.noreply.github.com>
2022-06-15 21:37:12 +1200

    Add files via upload

---
+++ javascript/navbar.js
@@ -0,0 +1,7 @@
+//nav bar javascript
+document.write('<nav class="navbar"><div><a href="index.html"><img class="logo" src="images\\zooLogofinal1.png" alt="Home"></a></div><ul class="nav-list"> ' + 
+    '<li class="list-item"><a href="tickets.html">Tickets</a></li>'+
+    '<li class="list-item"><a href="visitUs.html">Visit Us</a></li>'+
+    '<li class="list-item"><a href="contactUs.html">Contact Us</a></li>'+
+    '<li class="list-item"><a>Animals</a></li>'+
+    '<li class="list-item"><a>About Us</a></li></ul></nav>')
